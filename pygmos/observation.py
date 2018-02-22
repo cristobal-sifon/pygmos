23ea13363628e988e20243b3e1aea1ae55f409de
cristobal-sifon <sifon@astro.princeton.edu>
2018-02-22 17:00:19 -0500

    first implementation of slits_collection, still needs testing

--- pygmos/observation.py
+++ pygmos/observation.py
@@ -1,12 +1,16 @@
 from __future__ import absolute_import, division, print_function
 
+from astropy import units as u
 from astropy.io import fits
 from astropy.wcs import WCS
+import matplotlib
 from matplotlib import pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
 import sys
 
 
-class BaseMaskHeader(object):
+class BaseHeader(object):
 
     def __init__(self, file):
         self.file = file
@@ -24,22 +28,20 @@ class BaseMaskHeader(object):
             return hdr
 
 
-class BaseImage(object):
+class BaseImage(BaseHeader):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, file):
+        self.file = file
         self._data = None
-        #self.
+        super().__init__(self.file)
 
     @property
     def data(self):
         if self._data is None:
-            return fits.getdata(self.name)
+            return fits.getdata(self.file)
 
 
-
-class Mask(BaseMaskHeader):
-
+class Mask(BaseHeader):
     """
     Notes:
         -the position angle must be read from the preimage, although this will
@@ -55,11 +57,71 @@ class Mask(BaseMaskHeader):
         """
         self.file = file
         self.data = fits.getdata(self.file)
+        self._header = None
         if sys.version_info[0] == 2:
-            # by passing self to super, I don't need file in __init__() (?)
-            super(BaseMaskHeader, self).__init__()
+            super(BaseHeader, self).__init__()
         else:
             super().__init__(self.file)
+        self._nslits = None
+        self._pa = None
+        self._pixscale = None
+
+    @property
+    def nslits(self):
+        """Number of slits"""
+        if self._nslits is None:
+            return self.header[1]['NAXIS2']
+
+    @property
+    def pa(self):
+        """Mask position angle"""
+        if self._pa is None:
+            for h in self.header:
+                if 'MASK_PA' in h:
+                    return h['MASK_PA']
+            return None
+
+    @property
+    def pixscale(self):
+        """Pixel size in arcsec"""
+        if self._pixscale is None:
+            return self.header[1]['PIXSCALE'] * u.arcsec
+
+    def slits_collection(self, world=False, ax=None, **kwargs):
+        """Load mask slits as a `matplotlib.patches.PatchCollection` object
+
+        If `world=True`, slits will be returned in WCS coordinates. If
+        false, they will be returned in image coordinates.
+
+        `ax` should be a `matplotlib.axes.Axes` object. If provided, the
+        slits will be plotted in the axis.
+
+        `kwargs` are passed to `PatchCollection`
+
+        ***NOTE: only rectangle slits are implemented so far***
+        """
+        # slit sizes and positions are given in arcsec
+        if world:
+            keys = ('RA', 'DEC')
+            scale = 1 / 3600
+        else:
+            keys = ('x_ccd', 'y_ccd')
+            scale = 1 / self.pixscale.to(u.arcsec).value
+        h = self.header[1]
+        slits = []
+        x = self.data[keys[0]] + scale*self.data['slitpos_x']
+        y = self.data[keys[1]] + scale*self.data['slitpos_y']
+        for i in range(self.nslits):
+            if self.data['slittype'][i] == 'rectangle':
+                slits.append(
+                    Rectangle((x[i], y[i]),
+                              scale*self.data['slitsize_x'][i],
+                              scale*self.data['slitsize_y'][i],
+                              angle=self.pa))
+        collection = PatchCollection(slits, **kwargs)
+        if isinstance(ax, matplotlib.axes.Axes):
+            ax.add_collection(collection)
+        return collection
 
     def plot(self, ax=None):
         if ax is None:
